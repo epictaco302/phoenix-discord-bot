fc665b1756dd87d2c7cd9942a45ff718832e2bef
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-02-17 09:21:39 -0600

    add 8ball command

---
+++ commands/fun/8ball.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "8ball",
+  category: "fun",
+  description: "The Magic 8-ball, but it's on Discord.",
+  run: async (client, message, args) => {
+	const responses = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes – definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most Likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Reply hazy, try again",
+    "Very doubtful"
+
+	];
+
+	const randomIndex = Math.floor(Math.random() * responses.length);
+	message.channel.send(`:8ball: - ` + responses[randomIndex]);
+      }
+    };
