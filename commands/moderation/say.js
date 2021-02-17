01f309d8cf27504a54b5d79c151db5447933d387
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-02-17 08:12:57 -0600

    remove permission requirement for ph!say

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -8,9 +8,6 @@ module.exports = {
     run: (client, message, args) => {
         message.delete();
 
-        if (!message.member.hasPermission("MANAGE_MESSAGES"))
-            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
-
         if (args.length < 0)
             return message.reply("Nothing to say?").then(m => m.delete(5000));
 
