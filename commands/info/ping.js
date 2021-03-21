6f9c47648da7f872b764a01096bd402a7bf9e1a7
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-03-21 03:01:43 -0500

    add api ping

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -5,7 +5,6 @@ module.exports = {
     run: async (client, message, args) => {
         const msg = await message.channel.send(`🏓 Pinging....`);
 
-        msg.edit(`🏓 Pong!
-        Latency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms`);
+        msg.edit(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 }
