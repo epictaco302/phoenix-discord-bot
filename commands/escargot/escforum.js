43db8306e6131ad9c04ce92479ec6c9357afce6a
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-04-09 11:39:58 -0500

    remove escargot commands

--- commands/escargot/escforum.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "escforum",
-  category: "escargot",
-  aliases: ["forum", "escargotforum", "escnina"],
-  description: "Check out Escargot's discussion board on the NINA forums",
-  run: async (client, message, args) => {
-    message.channel.send(`Check out the Escargot discussion board on the NINA forums: https://forum.nina.bz/forum-40.html`)
-    }
-  }
