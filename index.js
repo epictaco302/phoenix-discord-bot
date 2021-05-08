3d0ddf8a4876313736b6ef900483174e635e7756
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-05-08 11:26:03 -0500

    TacoBot 1.4.0 (#16)
    Changelogs:
    
    - some backend improvements to add support for more commands
    - you can now warn users
    - added a snipe command, which reveals a deleted message after a user deleted it

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 const { Client, Collection } = require("discord.js");
-const { config } = require("dotenv");
 const { prefix, token } = require("./config.json");
-const fs = require("fs");
-
+const db = require("quick.db")
 const client = new Client({
-  disableEveryone: true
-});
-
-client.commands = new Collection();
-client.aliases = new Collection();
-
-client.categories = fs.readdirSync("./commands/");
-
-config({
-  path: __dirname + "/.env"
-});
-
-["command"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
+    disableEveryone: true
+})
+//_____________________________________________________________________________________________________________________________
 
 client.on("ready", () => {
   console.log(`Hi, ${client.user.username} is now online!`);
     client.user.setPresence({
         status: 'online',
         activity: {
-            name: "tb!help - version 1.3.0",
+            name: "tb!help - version 1.4.0",
             type: "WATCHING"
         }
     });
+//________________________________________________________________________________________________________________________________
+client.commands = new Collection();
+client.aliases = new Collection();
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
 });
 
-  client.on("message", async message => {
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
+
+  client.snipes.set(message.channel.id, {
+    content:message.content,
+    author:message.author.tag,
+    image:message.attachments.first() ? message.attachments.first().proxyURL : null
+  })
+
+})
+
+client.on("message", async message => {
+   let prefix = await db.fetch(`prefix_${message.guild.id}`)
+   if(prefix == null) {
+    prefix = "tb!"
+  }
+
+     let blacklist = await db.fetch(`blacklist_${message.author.id}`)
 
     if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
+
+    if (blacklist === "Blacklisted") return message.reply("You are blacklisted from the bot!")
+
+    // If message.member is uncached, cache it.
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -43,16 +56,17 @@ client.on("ready", () => {
 
     if (cmd.length === 0) return;
 
+    // Get the command
     let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
     if (!command) command = client.commands.get(client.aliases.get(cmd));
 
+    // If a command is finally found, run the command
     if (command)
-      command.run(client, message, args);
-  });
+        command.run(client, message, args);
 
-  client.on('shardError', error => {
-	console.error('A websocket connection encountered an error:', error);
+    });
+
 });
 
-// login to Discord with your app's token
 client.login(token);
