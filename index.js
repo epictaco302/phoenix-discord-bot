726f46a2d6ae7c8f2b9b1ab2164f01cbbda08545
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-05-29 22:07:32 -0500

    update version number to 1.4.1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
     client.user.setPresence({
         status: 'online',
         activity: {
-            name: "tb!help - version 1.4.0",
+            name: "tb!help - version 1.4.1 Prerelease",
             type: "WATCHING"
         }
     });
