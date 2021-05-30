97ec3fbdf40b7b10e8c07c565f147592bc1a212a
epictaco302 <74623886+epictaco302@users.noreply.github.com>
2021-05-29 22:27:15 -0500

    improved serverinfo by *a lot*

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -1,22 +1,52 @@
-const { MessageEmbed } = require('discord.js');
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'serverinfo',
-    description: 'Gives information about the server that it\'s executed in.',
-    category: 'info',
-    run: async(client, message) => {
-        if (!message.guild.members.cache.has(message.guild.ownerID)) await message.guild.members.fetch(message.guild.ownerID);
-        const embed = new MessageEmbed()
-            .setColor(0x00AE86)
-            .setTitle('Server Info')
-	      		.setThumbnail(message.guild.iconURL)
-		      	.addField(':arrow_right: Name', message.guild.name, true)
-		      	.addField(':arrow_right: ID', message.guild.id,   true)
-			.addField(':arrow_right: Region', message.guild.region.toUpperCase(), true)
-		      	.addField(':arrow_right: Creation Date', message.guild.createdAt.toDateString(), true)
-	      		.addField(':arrow_right: Owner', message.guild.owner.user.tag, true)
-	      		.addField(':arrow_right: Members', message.guild.memberCount, true)
-
-            message.channel.send(embed);
-    },
-};
+  name: "serverinfo",
+  description: "Gets info about the server it's run in",
+  usage: "serverinfo",
+  category: "info",
+  run: async (client, message, args) => {
+	  
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+    
+    
+        let region = {
+			
+        "brazil": ":flag_br: Brazil",
+        "eu-central": ":flag_eu: Central Europe",
+        "singapore": ":flag_sg: Singapore",
+        "us-central": ":flag_us: U.S. Central",
+        "sydney": ":flag_au: Sydney",
+        "us-east": ":flag_us: U.S. East",
+        "us-south": ":flag_us: U.S. South",
+        "us-west": ":flag_us: U.S. West",
+        "eu-west": ":flag_eu: Western Europe",
+        "vip-us-east": ":flag_us: VIP U.S. East",
+        "london": ":flag_gb: London",
+        "amsterdam": ":flag_nl: Amsterdam",
+        "hongkong": ":flag_hk: Hong Kong",
+        "russia": ":flag_ru: Russia",
+        "southafrica": ":flag_za:  South Africa"
+    };
+    const embed = new MessageEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .addField("Name", message.guild.name, true)
+        .addField("ID", message.guild.id, true)
+        .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+        .addField("Region", region[message.guild.region], true)
+        .addField("Total | Humans | Bots", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size} | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
+        .addField("Verification Level", message.guild.verificationLevel, true)
+        .addField("Channels", message.guild.channels.cache.size, true)
+        .addField("Roles", message.guild.roles.cache.size, true)     
+        .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+        .setThumbnail(message.guild.iconURL());
+       message.channel.send(embed);
+  }
+}
+        
